7bc3bd7f547b94433692617bdcab580bf88315fe
generalentropy <entropie@tuta.io>
2024-02-07 19:47:57 +0100

    overflow fix

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,13 +8,13 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="grid h-svh grid-rows-[auto_1fr_auto]">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
       <div className="overflow-scroll">
-        <main className="mx-auto max-w-3xl overflow-scroll">
+        <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
       </div>
