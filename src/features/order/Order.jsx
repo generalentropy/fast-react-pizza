bfa5cce502e48c5a8bab74819545091fbc48acb7
entropie <entropie@tuta.io>
2024-02-09 15:58:35 +0100

    Redux

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -52,7 +52,7 @@ function Order() {
         </p>
       </div>
 
-      <ul>
+      <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
           <OrderItem item={item} key={item.id} />
         ))}
